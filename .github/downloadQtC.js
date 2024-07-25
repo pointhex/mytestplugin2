c6a3a37a8814396af991369d5f0783161d6e8f82
Artem Sokolovskii <artem.sokolovskii@qt.io>
2024-07-25 10:57:04 +0200

    New download QtC script

--- .github/downloadQtC.js
+++ .github/downloadQtC.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const fs = require('fs');
 const { URL } = require('url');
+const { downloadPackage } = require('./utils');
 
 const version = process.argv[2];
 const platform = process.argv[3];
@@ -46,57 +47,6 @@ async function checkQtCreatorFolder(version) {
     return '';
 }
 
-async function downloadQtCreator(baseUrl, packageName) {
-    const url = baseUrl + packageName + '.7z';
-    const outputPath = packageName + '.7z';
-    return new Promise((resolve, reject) => {
-        function getFinalUrl(url) {
-            return new Promise((resolve, reject) => {
-                https.get(url, (response) => {
-                    if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
-                        // Handle redirection
-                        const redirectedUrl = new URL(response.headers.location, url).href;
-                        getFinalUrl(redirectedUrl).then(resolve).catch(reject);
-                    } else if (response.statusCode === 200) {
-                        resolve(url);
-                    } else {
-                        reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
-                    }
-                }).on('error', (err) => {
-                    reject(err);
-                });
-            });
-        }
-
-        getFinalUrl(url).then(finalUrl => {
-            https.get(finalUrl, (response) => {
-                if (response.statusCode !== 200) {
-                    return reject(new Error(`Failed to get '${finalUrl}' (${response.statusCode})`));
-                }
-
-                const file = fs.createWriteStream(outputPath);
-
-                response.pipe(file);
-
-                file.on('finish', () => {
-                    file.close(() => {
-                        console.log('Download complete');
-                        resolve();
-                    });
-                });
-
-                file.on('error', (err) => {
-                    fs.unlink(outputPath, () => {}); // Delete the file if there's an error
-                    reject(err);
-                });
-            }).on('error', (err) => {
-                reject(err);
-            });
-        }).catch(reject);
-    });
-}
-
-
 checkQtCreatorFolder(version).then(async(url) => {
     console.log(`Folder ${version} is ${url ? 'not empty' : 'empty'}`);
 
@@ -107,10 +57,9 @@ checkQtCreatorFolder(version).then(async(url) => {
 
     console.log(`Folder ${version}/${platform} is not empty. Proceeding to download.`);
     try {
-        for (const packageName of ['qtcreator', 'qtcreator_dev']) {
-            await downloadQtCreator(url, packageName);
+        for (const packageName of ['qtcreator.7z', 'qtcreator_dev.7z']) {
+            await downloadPackage(url, packageName);
         }
-        console.log('Download complete.');
         process.exit(0);
     } catch (error) {
         console.error('Download failed:', error);
---
+++ .github/utils.js
@@ -0,0 +1,55 @@
+const https = require('https');
+const fs = require('fs');
+async function downloadPackage(baseUrl, packageName) {
+    const url = baseUrl + packageName;
+    const outputPath = packageName;
+    return new Promise((resolve, reject) => {
+        function getFinalUrl(url) {
+            return new Promise((resolve, reject) => {
+                https.get(url, (response) => {
+                    if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
+                        // Handle redirection
+                        const redirectedUrl = new URL(response.headers.location, url).href;
+                        getFinalUrl(redirectedUrl).then(resolve).catch(reject);
+                    } else if (response.statusCode === 200) {
+                        resolve(url);
+                    } else {
+                        reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                    }
+                }).on('error', (err) => {
+                    reject(err);
+                });
+            });
+        }
+
+        getFinalUrl(url).then(finalUrl => {
+            https.get(finalUrl, (response) => {
+                if (response.statusCode !== 200) {
+                    return reject(new Error(`Failed to get '${finalUrl}' (${response.statusCode})`));
+                }
+
+                const file = fs.createWriteStream(outputPath);
+
+                response.pipe(file);
+
+                file.on('finish', () => {
+                    file.close(() => {
+                        console.log('Download complete for', outputPath);
+                        resolve();
+                    });
+                });
+
+                file.on('error', (err) => {
+                    fs.unlink(outputPath, () => {}); // Delete the file if there's an error
+                    reject(err);
+                });
+            }).on('error', (err) => {
+                reject(err);
+            });
+        }).catch(reject);
+    });
+}
+
+module.exports = {
+    downloadPackage
+};
