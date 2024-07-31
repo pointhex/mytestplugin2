3686c871f72c1cdcfb469eba1a047c5170ea2515
Artem Sokolovskii <artem.sokolovskii@qt.io>
2024-07-31 13:13:22 +0200

    Update registerPlugin.js

--- .github/scripts/registerPlugin.js
+++ .github/scripts/registerPlugin.js
@@ -1,62 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const pluginJson = `{
-  "status": "published",
-  "core_compat_version": "14.0.82",
-  "core_version": "14.0.82",
-  "host_os": "Windows",
-  "host_os_version": "10.0.0",
-  "host_os_architecture": "x86_64",
-  "plugins": [
-    {
-      "url": "",
-      "size": 5000,
-      "meta_data": {
-        "Category": "Core",
-        "Version": "0.0.1",
-        "CompatVersion": "0.0.1",
-        "Copyright": "(C) MyCompany",
-        "Name": "Myplugintest",
-        "Url": "https://www.mycompany.com",
-        "Vendor": "MyCompany",
-        "Description": "Put a short description of your plugin here",
-        "License": [
-          "Put short license information here"
-        ],
-        "Dependencies": [
-          {
-            "Name": "Core",
-            "Version": "14.0.82"
-          }
-        ]
-      },
-      "dependencies": []
-    }
-  ]
-}`;
-
-
-// Read inputs from environment variables or arguments
-const HTML_URL = process.env.HTML_URL || process.argv[2];
-const PLUGIN_NAME = process.env.PLUGIN_NAME || process.argv[3];
-const QT_CREATOR_VERSION = process.env.QT_CREATOR_VERSION || process.argv[4];
-const QT_CREATOR_VERSION_INTERNAL = process.env.QT_CREATOR_VERSION_INTERNAL || process.argv[5];
-const TOKEN = process.env.TOKEN || process.argv[6];
-
-// Read the main JSON files
-const templateFilePath = path.join(__dirname, 'plugin.json');
-const templateFileData = JSON.parse(fs.readFileSync(templateFilePath, 'utf8'));
-
-// Read the plugin JSON file
-const pluginFilePath = path.join(__dirname, '../..', `${PLUGIN_NAME}.json.in`);
-let pluginContent = fs.readFileSync(pluginFilePath, 'utf8');
-pluginContent = pluginContent.replace(/\${IDE_PLUGIN_DEPENDENCIES};?/g, '');
-pluginContent = pluginContent.replace(/,\s*}/g, '}');
-const pluginQtcData = JSON.parse(pluginContent);
-
 // Helper function to update plugin metadata
-const updatePluginMetadata = (plugin, pluginQtcData) => {
+const updatePluginMetadata = (plugin, pluginQtcData, env) => {
   plugin.meta_data.Name = pluginQtcData.Name;
   plugin.meta_data.Version = pluginQtcData.Version;
   plugin.meta_data.CompatVersion = pluginQtcData.CompatVersion;
@@ -68,28 +14,69 @@ const updatePluginMetadata = (plugin, pluginQtcData) => {
 
   plugin.meta_data.Dependencies.forEach(dependency => {
     if (dependency.Name === 'Core') {
-      dependency.Version = QT_CREATOR_VERSION_INTERNAL.split('-')[0];
+      dependency.Version = env.QT_CREATOR_VERSION_INTERNAL.split('-')[0];
     }
   });
 };
 
-const updatePluginData = (plugin, QtCVersion) => {
+const updatePluginData = (plugin, env, pluginQtcData) => {
   const dictionary_platform = {
-    'Windows': `${HTML_URL}/${PLUGIN_NAME}-${QT_CREATOR_VERSION}-Windows-x64.7z`,
-    'Linux': `${HTML_URL}/${PLUGIN_NAME}-${QT_CREATOR_VERSION}-Linux-x64.7z`,
-    'macOS': `${HTML_URL}/${PLUGIN_NAME}-${QT_CREATOR_VERSION}-macOS-x64.7z`
+    'Windows': `${env.HTML_URL}/${env.PLUGIN_NAME}-${env.QT_CREATOR_VERSION}-Windows-x64.7z`,
+    'Linux': `${env.HTML_URL}/${env.PLUGIN_NAME}-${env.QT_CREATOR_VERSION}-Linux-x64.7z`,
+    'macOS': `${env.HTML_URL}/${env.PLUGIN_NAME}-${env.QT_CREATOR_VERSION}-macOS-x64.7z`
   };
 
-  plugin.core_compat_version = QtCVersion
-  plugin.core_version = QtCVersion;
+  plugin.core_compat_version = env.QT_CREATOR_VERSION_INTERNAL;
+  plugin.core_version = env.QT_CREATOR_VERSION_INTERNAL;
 
   plugin.plugins.forEach(pluginsEntry => {
     pluginsEntry.url = dictionary_platform[plugin.host_os];
-    updatePluginMetadata(pluginsEntry, pluginQtcData);
+    updatePluginMetadata(pluginsEntry, pluginQtcData, env);
   });
+  return plugin;
 };
 
-const updateEndJsonData = (endJsonData, pluginQtcData) => {
+const createNewPluginData = (env, platform, pluginQtcData) => {
+  const pluginJson = {
+    "status": "published",
+    "core_compat_version": "14.0.82",
+    "core_version": "14.0.82",
+    "host_os": platform,
+    "host_os_version": "10.0.0",
+    "host_os_architecture": "x86_64",
+    "plugins": [
+      {
+        "url": "",
+        "size": 5000,
+        "meta_data": {
+          "Category": "Core",
+          "Version": "0.0.1",
+          "CompatVersion": "0.0.1",
+          "Copyright": "(C) MyCompany",
+          "Name": "Myplugintest",
+          "Url": "https://www.mycompany.com",
+          "Vendor": "MyCompany",
+          "Description": "Put a short description of your plugin here",
+          "License": [
+            "Put short license information here"
+          ],
+          "Dependencies": [
+            {
+              "Name": "Core",
+              "Version": "14.0.82"
+            }
+          ]
+        },
+        "dependencies": []
+      }
+    ]
+  };
+
+  updatePluginData(pluginJson, env, pluginQtcData);
+  return pluginJson;
+}
+
+const updateServerPluginJson = (endJsonData, pluginQtcData, env) => {
   // Update the global data in mainData
   endJsonData.name = pluginQtcData.Name;
   endJsonData.vendor = pluginQtcData.Vendor;
@@ -110,133 +97,90 @@ const updateEndJsonData = (endJsonData, pluginQtcData) => {
   let found = false;
   // Update or Add the plugin data for the current Qt Creator version
   for (const plugin of endJsonData.plugin_sets) {
-    if (plugin.core_compat_version === QT_CREATOR_VERSION_INTERNAL) {
-      updatePluginData(plugin, QT_CREATOR_VERSION_INTERNAL);
+    if (plugin.core_compat_version === env.QT_CREATOR_VERSION_INTERNAL) {
+      updatePluginData(plugin, env, pluginQtcData);
       found = true;
     }
   }
 
   if (!found)  {
     for (const platform of ['Windows', 'Linux', 'macOS']) {
-      const newPlugin = JSON.parse(pluginJson);
-      newPlugin.host_os = platform;
-      updatePluginData(newPlugin, QT_CREATOR_VERSION_INTERNAL);
-      endJsonData.plugin_sets.push(newPlugin);
+      endJsonData.plugin_sets.push(createNewPluginData(env, platform, pluginQtcData));
     }
   }
 
   // Save the updated JSON file
-  fs.writeFileSync(templateFilePath, JSON.stringify(endJsonData, null, 2), 'utf8');
+  const serverPluginJsonPath = path.join(__dirname, `${env.PLUGIN_NAME}.json`);
+  fs.writeFileSync(serverPluginJsonPath, JSON.stringify(endJsonData, null, 2), 'utf8');
 };
 
-const makeGetRequest = async (url, token) => {
+const request = async (type, url, token, data) => {
   const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'accept': 'application/json'
-    }
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  return await response.json();
-};
-
-// Function to make a PUT request
-const makePutRequest = async (url, data, token) => {
-  const response = await fetch(url, {
-    method: 'PUT',
+    method: type,
     headers: {
       'Authorization': `Bearer ${token}`,
       'accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data)
+    body: data ? JSON.stringify(data) : undefined
   });
-
   if (!response.ok) {
     const errorResponse = await response.json();
-    console.error('PUT Request Error Response:', errorResponse); // Log the error response    
+    console.error('PUT Request Error Response:', errorResponse); // Log the error response
     throw new Error(`HTTP error! status: ${response.status}`);
   }
-
   return await response.json();
-};
-
-const makePostRequest = async (url, data, token) => {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  // return await response.json();
-  const contentType = response.headers.
-    get('content-type');
-  if (contentType && contentType.
-    includes('application/json')) {
-    return response.json();
-  } else {
-    return {};
-  }
-};
-
-const API_URL = '';
-if (API_URL === '') {
-  updateEndJsonData(templateFileData, pluginQtcData);
-  process.exit(0);
 }
 
-const purgCache = async () => {
+const put = (url, token, data) => request('PUT', url, token, data)
+const post = (url, token, data) => request('POST', url, token, data)
+const get = (url, token) => request('GET', url, token)
+
+const purgeCache = async (env) => {
   try {
-    await makePostRequest(
-      `${API_URL}api/v1/cache/purgeall`,
-      {},
-      TOKEN
-    );
+    await post(`${env.API_URL}api/v1/cache/purgeall`, {}, env.TOKEN);
     console.log('Cache purged successfully');
   } catch (error) {
     console.error('Error:', error);
   }
 };
 
-const url = `${API_URL}api/v1/admin/extensions?search=${PLUGIN_NAME}`;
-makeGetRequest(url, TOKEN)
-  .then(response => {
-    if (response.items.length > 0 && response.items[0].extension_id !== '') {
-      const pluginId = response.items[0].extension_id;
-      console.log('Plugin found. Updating the plugin');
-      updateEndJsonData(response.items[0], pluginQtcData);
-      makePutRequest(
-        `${API_URL}api/v1/admin/extensions/${pluginId}`, 
-        response.items[0], 
-        TOKEN
-      )
-      .then(() => purgCache())
-      .catch(error => console.error('Error:', error));
-    } else {
-      console.log('No plugin found. Creating a new plugin');
-      updateEndJsonData(templateFileData, pluginQtcData);
-      makePostRequest(
-        `${API_URL}api/v1/admin/extensions`, 
-        templateFileData, 
-        TOKEN
-      )
-      .then(() => purgCache())
-      .catch(error => console.error('Error:', error));
-    }
-  })
-  .catch(error => console.error('Error:', error));
+async function main() {
+  const env = {
+    HTML_URL: process.env.HTML_URL || process.argv[2],
+    PLUGIN_NAME: process.env.PLUGIN_NAME || process.argv[3],
+    QT_CREATOR_VERSION: process.env.QT_CREATOR_VERSION || process.argv[4],
+    QT_CREATOR_VERSION_INTERNAL: process.env.QT_CREATOR_VERSION_INTERNAL || process.argv[5],
+    TOKEN: process.env.TOKEN || process.argv[6],
+    API_URL: process.env.API_URL || process.argv[7] || ''
+  };
 
-console.log('JSON file updated successfully');
+  // Read the plugin JSON file
+  const pluginFilePath = path.join(__dirname, '../..', `${env.PLUGIN_NAME}.json.in`);
+  let pluginContent = fs.readFileSync(pluginFilePath, 'utf8');
+  pluginContent = pluginContent.replace(/\${IDE_PLUGIN_DEPENDENCIES};?/g, '');
+  pluginContent = pluginContent.replace(/,\s*}/g, '}');
+  const pluginQtcData = JSON.parse(pluginContent);
+
+  const templateFileData = require('./plugin.json');
+  
+  if (env.API_URL === '') {
+    updateServerPluginJson(templateFileData, pluginQtcData, env);
+    process.exit(0);
+  }
+
+  const response = await get(`${api_url}api/v1/admin/extensions?search=${env.PLUGIN_NAME}`, env.TOKEN);
+  if (response.items.length > 0 && response.items[0].extension_id !== '') {
+    const pluginId = response.items[0].extension_id;
+    console.log('Plugin found. Updating the plugin');
+    updateServerPluginJson(response.items[0], pluginQtcData, env);
+    await put(`${api_url}api/v1/admin/extensions/${pluginId}`, response.items[0], env.TOKEN);
+  } else {
+    console.log('No plugin found. Creating a new plugin');
+    updateServerPluginJson(templateFileData, pluginQtcData, env);
+    await post(`${api_url}api/v1/admin/extensions`, templateFileData, env.TOKEN);
+  }
+  await purgeCache(env);
+}
+
+main().then(() => console.log('JSON file updated successfully'));
